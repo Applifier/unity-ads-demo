76e5f96a79ec9454fd09e1620c56e450e1c8a8ac
Nikkolai Davenport <nikkolai@unity3d.com>
2015-04-24 13:49:34 -0700

    Update level number format to 2 digits

--- Assets/SpaceAds/Scripts/Constants.cs
+++ Assets/SpaceAds/Scripts/Constants.cs
@@ -4,6 +4,6 @@
 
 	public const string KEY_REWARD_COOLDOWN_TIME = "RewardCooldownTime";
 
-	public const string KEYFORMAT_LEVEL_UNLOCKED = "Level{0}_Unlocked";
-	public const string KEYFORMAT_LEVEL_HIGHSCORE = "Level{0}_HighScore";
+	public const string KEYFORMAT_LEVEL_UNLOCKED = "Level{0:00}_Unlocked";
+	public const string KEYFORMAT_LEVEL_HIGHSCORE = "Level{0:00}_HighScore";
 }
